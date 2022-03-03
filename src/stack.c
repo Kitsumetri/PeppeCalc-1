8baf0b3f6ff5e5eeb2164f04eda1acb576a20cbf
grafvontee <grafvontee@yandex.ru>
2022-03-03 18:51:23 +0300

    Added stack_type in Stack Structure

--- src/stack.c
+++ src/stack.c
@@ -4,13 +4,13 @@
 Stack make_stack() {
     Stack new_stack;
     new_stack.length = 0;
-    new_stack.elems = (int*)calloc(0, sizeof(int));
+    new_stack.elems = (stack_type *)calloc(0, sizeof(stack_type));
     return new_stack;
 }
 
-void push_to_stack(Stack *stake, int new_value) {
+void push_to_stack(Stack *stake, stack_type new_value) {
     stake->length += 1;
-    int *temp = (int*)malloc(stake->length * sizeof(int));
+    stack_type *temp = (stack_type *)malloc(stake->length * sizeof(stack_type));
     for (int i = 0; i < stake->length - 1; i++)
         temp[i] = stake->elems[i];
     temp[stake->length - 1] = new_value;
@@ -22,9 +22,9 @@ void pop_from_stack(Stack *stake) {
     if (stake->length == 0)
         return;
     stake->length -= 1;
-    stake->elems = (int *) realloc(stake->elems, stake->length * sizeof(int));
+    stake->elems = (stack_type *) realloc(stake->elems, stake->length * sizeof(stack_type));
 }
 
-const int* get_head(Stack *stake) {
+const stack_type * get_head(Stack *stake) {
     return ((stake->length == 0) ? NULL : &stake->elems[stake->length - 1]);
 }
--- src/stack.h
+++ src/stack.h
@@ -1,11 +1,12 @@
 #pragma once
 
+typedef double stack_type;
 typedef struct {
-    int *elems;
+    stack_type *elems;
     int length;
 } Stack;
 
 Stack make_stack();
-void push_to_stack(Stack *stake, int new_value);
+void push_to_stack(Stack *stake, stack_type new_value);
 void pop_from_stack(Stack *stake);
-const int* get_head(Stack *stake);
+const stack_type * get_head(Stack *stake);
