bc02616b7cfd8b76653eb624ebdbe14d81d1c9e4
Kitsu <krasilnikovkolya4654@gmail.com>
2022-03-04 13:04:19 +0300

    Added get_str functions

--- src/get_str.c
+++ src/get_str.c
@@ -14,30 +14,15 @@ char *get_string_from_input() {
 
 char *get_new_str() {
     const char keys[] = "()+-*^/";
-    const char functions[][5] = {"sin", "cos", "log", "sqrt"};
     char *input_str = get_string_from_input();
     char *new_str = (char*) calloc(LIMIT_SIZE, sizeof(char));
-    int i = 0; int j = 0; int flag;
+    int i = 0; int j = 0;
 
     while (i < strlen(input_str))
     {
         new_str[j++] = input_str[i];
         if (strchr(keys,input_str[i+1]) || strchr(keys,input_str[i]))
             new_str[j++] = ' ';
-
-        for (int k = 0; k < 4; ++k)
-        {
-            if (strstr(new_str, functions[k]) && input_str[i+1] == '(')
-            {
-                i++;
-                flag = 1;
-            }
-        }
-        if(flag == 1 && input_str[i+1] == ')')
-        {
-            i++;
-            flag = 0;
-        }
         i++;
     }
     return new_str;
--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include "get_str.h"
 
-int main() {
+int main()
+{
+    printf("%s\n", get_new_str());
     return 0;
 }
